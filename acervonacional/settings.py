2f0bd3f24e78ae53b3832f3e20f3bf5e242cf1aa
AngelaGarces <angelacontapositivo@gmail.com>
2021-12-03 18:03:53 -0300

    configurações iniciais

--- acervonacional/settings.py
+++ acervonacional/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&5o@ogl3wjx(2uf+7#%4_-xotkezjh-ow+lb%#y*4jxy-vv5qd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'acervonacional.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
