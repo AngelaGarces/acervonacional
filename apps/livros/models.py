b077c901b2e49c2c422d6f9b1fd92549ff191d49
AngelaGarces <angelacontapositivo@gmail.com>
2021-12-10 17:50:50 -0300

    configurando os módulos iniciais

--- apps/livros/models.py
+++ apps/livros/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class livros(models.Model):
+
+    id = models.AutoField(primary_key=True, db_column='id')
+    nome = models.CharField(default='', max_length= 200)
