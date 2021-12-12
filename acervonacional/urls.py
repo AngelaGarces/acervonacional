eff19a4076dbbe482efc0b2184a7338f54f88f90
AngelaGarces <angelacontapositivo@gmail.com>
2021-12-12 09:05:34 -0300

    criação de home

--- acervonacional/urls.py
+++ acervonacional/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 from apps.livros import urls as livros_urls
+from apps.home import urls as home_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('listar/', include(livros_urls)),
+    path('', include(home_urls)),
 ]
---
+++ apps/home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import HomeView
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('home/', HomeView.as_view(), name='home'), 
+]
